798160e196fd307022bd7f821f7dd79d1ccac43f
bruin <bruin@m2semi.com>
2023-09-12 08:23:23 +0000

    add a SPACE after the pin name

--- vivado/rtl/pin_uart.v
+++ vivado/rtl/pin_uart.v
@@ -45,18 +45,26 @@ reg [63:0] data;
 
 initial begin
     data = {64{1'b1}};
+
     data[0] = 1'b0;
     data[8:1] = (NAME >> 24) & 8'hff;
     data[9] = 1'b1;
+
     data[10] = 1'b0;
     data[18:11] = (NAME >> 16) & 8'hff;
     data[19] = 1'b1;
+
     data[20] = 1'b0;
     data[28:21] = (NAME >> 8) & 8'hff;
     data[29] = 1'b1;
+
     data[30] = 1'b0;
     data[38:31] = (NAME >> 0) & 8'hff;
     data[39] = 1'b1;
+
+    data[40] = 1'b0;
+    data[48:41] = 8'h20; // space
+    data[49] = 1'b1;
 end
 
 reg out_reg = 1'b1;
